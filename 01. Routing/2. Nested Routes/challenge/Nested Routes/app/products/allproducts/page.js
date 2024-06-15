fe177e921b9d93facf8af6f91cb788e0a29712a9
manishnangliya <manunangliya@gmail.com>
2024-06-16 02:13:23 +0530

    complete 2. nested routes

---
+++ 01. Routing/2. Nested Routes/challenge/Nested Routes/app/products/allproducts/page.js
@@ -0,0 +1,10 @@
+
+const allProduct = () => {
+  return (
+    <div>
+      (1220) products in the store
+    </div>
+  )
+}
+
+export default allProduct
---
+++ 01. Routing/2. Nested Routes/challenge/Nested Routes/app/products/specificproduct/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Here is my one & only product
+    </div>
+  )
+}
+
+export default page
